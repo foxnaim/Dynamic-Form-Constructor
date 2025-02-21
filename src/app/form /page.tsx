055c2bdf2901922e904456060759261c93149b09
Rick <118506963+foxnaim@users.noreply.github.com>
2025-02-21 21:52:45 +0500

    modified:   package.json 	modified:   src/app/form /page.tsx 	modified:   src/app/page.tsx 	deleted:    src/app/pages/Home/page.tsx 	modified:   yarn.lock

--- src/app/form /page.tsx
+++ src/app/form /page.tsx
@@ -1,16 +1,14 @@
 import FormRenderer from "@/app/components/FormRenderer/FormRenderer";
+import Link from "next/link";
 
 export default function FormPage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6">
       <h1 className="text-2xl font-bold mb-4">Заполнение формы</h1>
       <FormRenderer />
-      <a
-        href="/"
-        className="mt-4 text-blue-500 hover:underline text-lg"
-      >
+      <Link href="/" className="mt-4 text-blue-500 hover:underline text-lg">
         ← Назад
-      </a>
+      </Link>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import App from './pages/Home/page'
+"use client";
+
+import FormBuilder from "@/app/components/FormBuilder/FormBuilder";
+import Link from "next/link";
 
 
+export default function Home() {
 
 
-const Home = () => {
   return (
-    <App/>
-  )
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6">
+      <h1 className="text-2xl font-bold mb-4">Конструктор форм</h1>
+      <FormBuilder />
+      
+      <Link href="/form"   className="mt-4 text-blue-500 hover:underline text-lg"
+      >
+        Перейти к заполнению →
+      </Link>
+    </div>
+  );
 }
-
-export default Home
